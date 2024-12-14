07ae67e4e69e458ba435fb6693a96f1b83f7c5ec
arnold7800x3d <arnold.7800x3d@gmail.com>
2024-12-15 00:01:17 +0300

    http get request python file

---
+++ http_GET_request.py
@@ -0,0 +1,14 @@
+import requests
+
+def check_website(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            print(f"Successfully connected to {url}")
+            print(f"Server: {response.headers.get('Server')}")
+        else:
+            print(f"Failed to connect to {url}. Status code: {response.status_code}")
+    except requests.exceptions.RequestException as e:
+        print(f"An error occured: {e}")
+
+check_website("https://9animetv.to/")
